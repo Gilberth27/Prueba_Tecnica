36c50f01c5a832fafd5edf60deb209f3f136bf91
Gilberth Nick CRuz Gonzalez <42789447+Gilberth27@users.noreply.github.com>
2021-10-12 21:38:02 -0500

    Configure Update-delete and search modules

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func conec() (conexion *sql.DB) {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println("conextado")
+	fmt.Println("conectado")
 	return conexion
 
 }
@@ -37,33 +37,107 @@ type tickets struct {
 	ESTADO  string `json:"ESTADO"`
 }
 
-type allTask []tickets
-
-var Tickets = allTask{
-	{
-		ID:      1,
-		USUARIO: "Gilberth",
-		FCH_CRE: "11-10-2021",
-		FCH_UPD: "12-10-2021",
-		ESTADO:  "activo",
-	},
-}
-
 func Mainlink(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Esta es Mi prueba tecnicaaaaa")
 }
 
-func ConsultaTickets(w http.ResponseWriter, r *http.Request) {
-	modelo.ExecuteTemplate(w, "consultar", nil)
-
+type Caso struct {
+	ID        int
+	USUARIO   string
+	FECHA_CRE string
+	FECHA_UPD string
+	ESTADO    string
 }
+
 func CrearTickets(w http.ResponseWriter, r *http.Request) {
 	modelo.ExecuteTemplate(w, "Crear", nil)
 }
 
-func ActualizarTickets(w http.ResponseWriter, r *http.Request) {
-	modelo.ExecuteTemplate(w, "consultar", nil)
+func UpdateTickets(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		usuario := r.FormValue("usuario")
+		creacion := r.FormValue("creacion")
+		actualizar := r.FormValue("actualizar")
+		estado := r.FormValue("estado")
+		Correcta := conec()
+		UPDATECasos, err := Correcta.Prepare("UPDATE casos SET USUARIO=?,FECHA_CRE=?,FECHA_UPD=?,ESTADO=? WHERE ID=?")
+
+		if err != nil {
+			panic((err.Error()))
+		}
+		UPDATECasos.Exec(usuario, creacion, actualizar, estado, id)
+		http.Redirect(w, r, "/consultar", 301)
+	}
+}
+
+func ModificarTickets(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("ID")
+	fmt.Println(ID)
+	Correcta := conec()
+	EditarCasos, err := Correcta.Query("SELECT * FROM casos where ID=?", ID)
+	caso := Caso{}
+	for EditarCasos.Next() {
+		var ID int
+		var USUARIO, FECHA_CRE, FECHA_UPD, ESTADO string
+
+		err = EditarCasos.Scan(&ID, &USUARIO, &FECHA_CRE, &FECHA_UPD, &ESTADO)
+		if err != nil {
+			panic(err.Error())
+		}
+		caso.ID = ID
+		caso.USUARIO = USUARIO
+		caso.FECHA_CRE = FECHA_CRE
+		caso.FECHA_UPD = FECHA_UPD
+		caso.ESTADO = ESTADO
+	}
+	fmt.Println(caso)
+	modelo.ExecuteTemplate(w, "editar", caso)
+}
+func EliminarTickets(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("ID")
+	fmt.Println(ID)
+
+	Correcta := conec()
+	EliminarCasos, err := Correcta.Prepare("DELETE FROM casos where ID=?")
+	if err != nil {
+		panic((err.Error()))
+	}
+	EliminarCasos.Exec(ID)
+	http.Redirect(w, r, "/consultar", 301)
+}
+
+func ConsultaTickets(w http.ResponseWriter, r *http.Request) {
+
+	Correcta := conec()
+	ConsultarCasos, err := Correcta.Query("SELECT * FROM casos")
+
+	if err != nil {
+		panic((err.Error()))
+	}
+	caso := Caso{}
+	Acaso := []Caso{}
+
+	for ConsultarCasos.Next() {
+		var ID int
+		var USUARIO, FECHA_CRE, FECHA_UPD, ESTADO string
+
+		err = ConsultarCasos.Scan(&ID, &USUARIO, &FECHA_CRE, &FECHA_UPD, &ESTADO)
+		if err != nil {
+			panic(err.Error())
+		}
+		caso.ID = ID
+		caso.USUARIO = USUARIO
+		caso.FECHA_CRE = FECHA_CRE
+		caso.FECHA_UPD = FECHA_UPD
+		caso.ESTADO = ESTADO
+
+		Acaso = append(Acaso, caso)
+
+	}
+
+	modelo.ExecuteTemplate(w, "consultar", Acaso)
 }
 
 func RegistrarTickets(w http.ResponseWriter, r *http.Request) {
@@ -80,7 +154,7 @@ func RegistrarTickets(w http.ResponseWriter, r *http.Request) {
 			panic((err.Error()))
 		}
 		insertarCasos.Exec(usuario, creacion, actualizar, estado)
-		http.Redirect(w, r, "/", 301)
+		http.Redirect(w, r, "/consultar", 301)
 	}
 }
 
@@ -91,6 +165,9 @@ func main() {
 	link.HandleFunc("/consultar", ConsultaTickets)
 	link.HandleFunc("/crear", CrearTickets)
 	link.HandleFunc("/registrar", RegistrarTickets)
+	link.HandleFunc("/eliminar", EliminarTickets)
+	link.HandleFunc("/editar", ModificarTickets)
+	link.HandleFunc("/actualizar", UpdateTickets)
 	log.Println("Conexion ejecutandose")
 
 	http.ListenAndServe(":5000", link)
